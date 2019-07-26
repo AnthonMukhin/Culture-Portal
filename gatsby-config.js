97cfdd086a4f199786b52132e15c82fad97c651f
yauhenkviatkouski <kvetkovski@gmail.com>
2019-07-26 23:11:30 +0300

    test commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `t56g6tagj34t`,
+        accessToken: `HXg2kVNAan9V3JBJz-K7juG0Bb6T0Rcytrotl_QbTQ8`,
+      },
+    },
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,55 @@
-import React from "react"
+import React from "react";
+import { graphql } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+
+export const query = graphql`
+query MyQuery {
+  __typename
+  contentfulWriter {
+    name
+    yearsOfLife
+    youTubeLink
+    location {
+      lat
+      lon
+    }
+    photoGallery {
+      internal {
+        content
+      }
+    }
+    photoWriter {
+      file {
+        url
+      }
+    }
+    listOfArtistsWorks {
+      internal {
+        content
+      }
+    }
+  }
+}
+
+`
+
+export default ({data}) => (
+  <>
+    <div>Имя: {JSON.stringify(data.contentfulWriter.name)}</div>
+    <div>Годы жизни: {JSON.stringify(data.contentfulWriter.yearsOfLife)}</div>
+    <div>Видео (должно быть модальным):</div>
+    <iframe
+      title="writerVideo"
+      width="560"
+      height="315"
+      src={data.contentfulWriter.youTubeLink}
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      >
+    </iframe>
+    <br/>
+    пример данных по писателю, размещаемых на Contentfull:
+    <p>{JSON.stringify(data.contentfulWriter)}</p>
+  </>
+)
+
+
