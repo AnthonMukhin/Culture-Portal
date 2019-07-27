df45e1f5e2fd6ea309ea8ebbb6f84663c33f2a86
yauhenkviatkouski <kvetkovski@gmail.com>
2019-07-27 15:40:35 +0300

    add bootstrap

---
+++ src/pages/page2.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import '../styles/bootstrap.min.css';
+
+export default () => (
+  <div>
+      <p>This contains a button which uses the bootstrap.css</p>
+      <button class="btn btn-primary">Click me</button>
+  </div>
+);
