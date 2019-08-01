991a94b61bf702bed18a51ccebcf9c031968a9db
yauhenkviatkouski <kvetkovski@gmail.com>
2019-08-01 18:39:14 +0300

    feat: add data query to layout component

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,13 +2,76 @@ import React from "react";
 import Helmet from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import '../../utils/i18next';
+import { useStaticQuery } from "gatsby"
 
 import styles from './layout.module.css';
 import MainNavigation from '../../components/main-navigation/main-navigation';
 import LanguageSelector from '../../components/language-selector/language-selector';
 
 export default (props) => {
-const authorsArray = props.authorsArray;
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allContentfulAuthor {
+          totalCount
+          nodes {
+            id
+            name {
+              by
+              en
+              ru
+            }
+            summary {
+              by
+              en
+              ru
+            }
+            placeOfBirth {
+              by
+              en
+              ru
+            }
+            yearsOfLife
+            avatar {
+              file {
+                url
+              }
+            }
+            biographyTimeline {
+              date
+              event {
+                by
+                en
+                ru
+              }
+            }
+            listOfArtistsWorks {
+              date
+              event {
+                by
+                en
+                ru
+              }
+            }
+            photoGallery {
+              link
+              description {
+                by
+                en
+                ru
+              }
+            }
+            videoLink
+            placeOfActivity {
+              lat
+              lon
+            }
+          }
+        }
+      }
+    `
+  )
+  const authorsArray = data.allContentfulAuthor.nodes;
   const { t } = useTranslation('layout');
   return (
     <>
@@ -31,3 +94,5 @@ const authorsArray = props.authorsArray;
     </>
   );
 }
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,12 @@ import React from "react";
 
 import Layout from "../components/layout/layout";
 
-export default (query) => {
-  const authorsArray = query.data.allContentfulAuthor.nodes;
-  // const teamArray = query.data.allContentfulTeam.nodes;
-  console.log(authorsArray);
-  // console.log(teamArray);
+export default ({data}) => {
+  const authorsArraySummary = data.allContentfulAuthor.nodes;
+  console.log("from index :");
+  console.log(authorsArraySummary);
   return (
-    <Layout authorsArray={authorsArray}></Layout>
+    <Layout></Layout>
   );
 };
 
@@ -17,56 +16,20 @@ query MyQuery {
   allContentfulAuthor {
     totalCount
     nodes {
-      id
-      name {
-        by
-        en
-        ru
+      avatar {
+        file {
+          url
+        }
       }
       summary {
         by
         en
         ru
       }
-      placeOfBirth {
+      name {
         by
         en
         ru
-      }
-      yearsOfLife
-      avatar {
-        file {
-          url
-        }
-      }
-      biographyTimeline {
-        date
-        event {
-          by
-          en
-          ru
-        }
-      }
-      listOfArtistsWorks {
-        date
-        event {
-          by
-          en
-          ru
-        }
-      }
-      photoGallery {
-        link
-        description {
-          by
-          en
-          ru
-        }
-      }
-      videoLink
-      placeOfActivity {
-        lat
-        lon
       }
     }
   }
