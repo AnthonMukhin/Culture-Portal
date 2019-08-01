76e598c2b60d1624d03d18a31bf0161642210005
yauhenkviatkouski <kvetkovski@gmail.com>
2019-08-01 17:02:01 +0300

    fix: video, delete video title from writer page

--- src/pages/writer.js
+++ src/pages/writer.js
@@ -2,18 +2,15 @@ import React from 'react';
 
 import Layout from '../components/layout/layout';
 import video from './../components/video-modal/video';
-import { useTranslation } from 'react-i18next';
 import '../utils/i18next';
 
 export default ({location}) => {
+  const writer = location.state;
   console.log('ONE WRITER:');
-  console.log(location.state);
-  const { t } = useTranslation('layout');
-  const videoLink ="https://www.youtube.com/embed/GNvCm0tK9gw";
+  console.log(writer);
   return (
     <Layout>
-      <div>{t('watch')}</div>
-      {video(videoLink)}
+      {video(writer.videoLink)}
     </Layout>
   );
 };
