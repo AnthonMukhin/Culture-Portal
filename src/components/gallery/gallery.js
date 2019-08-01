293be7027528a48815faa5eaef4e1f18f0126787
BarikQ <yar.barik99@gmail.com>
2019-08-01 19:20:34 +0300

    feat: add thumbnails navigation

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -3,15 +3,31 @@ import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
 export default class Carousel extends React.Component {
+  items = [1, 2, 3, 4, 5];
+
   state = {
+    currentIndex: 0,
     pictures: []
   };
+
+  slideTo = i => this.setState({ currentIndex: i });
+
+  onSlideChanged = e => this.setState({ currentIndex: e.item });
+
+  slideNext = () =>
+    this.setState({ currentIndex: this.state.currentIndex + 1 });
+
+  slidePrev = () =>
+    this.setState({ currentIndex: this.state.currentIndex - 1 });
+
+  thumbItem = (item, i) => {
+    return <img src={item.key} class="gallery-item" alt="" onClick={() => this.slideTo(i)} />;
+  }
 
   componentDidMount() {
     const data = Array(7)
       .fill()
       .map((item, i) => `http://placehold.it/300x100`);
-      // console.log(JSON.parse(this.props.authorData[0].internal.content).link);
 
     Promise.resolve(data).then(result => {
       const pictures = this.renderImages(result);
@@ -35,17 +51,19 @@ export default class Carousel extends React.Component {
   }
 
   render() {
-    const stagePadding = {
-      paddingLeft: 50,
-      paddingRight: 100
-    };
+    const { currentIndex } = this.state;
     return (
-      <AliceCarousel
-        mouseDragEnabled
+      <div>
+        <AliceCarousel
+          items={this.state.pictures}
+          slideToIndex={currentIndex}
+          onSlideChanged={this.onSlideChanged}
+          mouseDragEnabled
         duration={400}
-        stagePadding={stagePadding}
-        items={this.state.pictures}
-      />
+        />
+
+        <ul class="gallery-items">{this.state.pictures.map(this.thumbItem)}</ul>
+      </div>
     );
   }
 }
--- src/pages/writer.js
+++ src/pages/writer.js
@@ -3,6 +3,8 @@ import React from "react";
 import Layout from "../components/layout/layout";
 import Gallery from '../components/gallery/gallery';
 
+import '../components/gallery/gallery.css';
+
 export default () => {
   return (
     <Layout>
