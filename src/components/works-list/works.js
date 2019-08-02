a7d7f78ed368b810d26a11975afcd62b33e4a377
BarikQ <yar.barik99@gmail.com>
2019-08-02 19:25:34 +0300

    feat: add works list

---
+++ src/components/works-list/works.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useTranslation } from 'react-i18next';
+import '../../utils/i18next';
+
+import '../../styles/bootstrap.min.css'
+import './works.css';
+
+export default (worksList) => {
+
+  const { i18n } = useTranslation();
+  const { t } = useTranslation('works');
+  const currentLang = i18n.language;
+
+  const worksContainer = () => {
+    const list = worksList.map((elem, index) => {
+      return (
+      <li className="work">
+        {elem.date}&nbsp; — &nbsp;{elem.event[currentLang]}
+      </li>
+      );
+    });
+
+    return (
+      <ul className="works-list">
+        {list}
+      </ul>
+    );
+  }
+
+  return (
+    <div className="container">
+      <h3>{t('title')}</h3>
+      {worksContainer()}
+    </div>
+  );
+}
