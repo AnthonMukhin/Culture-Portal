d78852a02e5834f20782e744f1540fc4f488f189
AlenaShkr <elalshkr@gmail.com>
2019-08-03 06:24:16 +0300

    cult-portal/writeroftheDay/feat: add random method choice writer and add on page index

--- src/components/writerOfTheDay/writerOfTheDay.js
+++ src/components/writerOfTheDay/writerOfTheDay.js
@@ -1,23 +1,24 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import { useTranslation } from "react-i18next"
-import "../../utils/i18next"
+import { useTranslation } from "react-i18next";
+import "../../utils/i18next";
 
-import "./writerOfTheDay.css"
+import '../../styles/bootstrap.min.css'
+import "./writerOfTheDay.css";
 
 export default dataWriter => {
-  const { i18n } = useTranslation()
-  const currentLanguage = i18n.language
-  const { t } = useTranslation("writerOfTheDay")
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language;
+  const { t } = useTranslation("writerOfTheDay");
 
-  const data = dataWriter
+  const data = dataWriter;
 
-  const index = Math.round(Math.random() * data.lenght).toString()
-  const nameForUrl = data[index].name.en.replace(/\s/g, "")
+  const index = (Math.floor(Math.random() * (data.length))).toString();
+  const nameForUrl = data[index].name.en.replace(/\s/g, "");
   return (
     <>
-      <div className="writer-day">
+      <div className="container">
         <h3>{t("title")}:</h3>
 
         <img
@@ -28,7 +29,7 @@ export default dataWriter => {
         <h3>{data[index].name[currentLanguage]}</h3>
         <p>{data[index].summary[currentLanguage]}</p>
         <Link to={`/writer/${nameForUrl}`}>
-          <button>{t("button")}</button>
+          <button className="btn-primary">{t("button")}</button>
         </Link>
       </div>
     </>
