3b37639da30407fa5810365d4510037040bfd2a1
Anthon_M <xll2005@tut.by>
2019-08-04 22:10:27 +0300

    add: change language

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -80,7 +80,7 @@ export default ({children}) => {
   return (
     <>
       <Helmet>
-        <title>Писатели</title>
+        <title>{t('header')}</title>
       </Helmet>
       <header className='row justify-content-around align-items-center'>
         <h1 className='col-3'>{t('title')}</h1>
